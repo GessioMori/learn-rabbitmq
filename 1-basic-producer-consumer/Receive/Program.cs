d1f3d06bc257878b1a30b61eceb614d98a8e1715
Gessio Mori Neto <gessiomorin@gmail.com>
2023-09-11 17:18:11 -0300

    Creates a basic Consumer/Producer

---
+++ 1-basic-producer-consumer/Receive/Program.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory connectionFactory = new ConnectionFactory
+{
+    HostName = "localhost",
+    Port = 5672,
+};
+
+using IConnection connection = connectionFactory.CreateConnection();
+
+using IModel channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "hello-world",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+Console.WriteLine(" [*] Waiting for messages.");
+
+EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, ea) =>
+{
+    byte[] body = ea.Body.ToArray();
+    string message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received {message}");
+};
+
+channel.BasicConsume(queue: "hello-world",
+                     autoAck: true,
+                     consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+
+Console.ReadLine();
--- 1-basic-producer-consumer/Send/Program.cs
+++ 1-basic-producer-consumer/Send/Program.cs
@@ -1,1 +1,34 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory connectionFactory = new ConnectionFactory
+{
+    HostName = "localhost",
+};
+
+using IConnection connection = connectionFactory.CreateConnection();
+
+using IModel channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "hello-world",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+const string message = "This is the first message.";
+
+byte[] body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(
+    exchange: String.Empty,
+    routingKey: "hello-world",
+    basicProperties: null,
+    body: body);
+
+Console.WriteLine($" [x] Sent {message}");
+
+Console.WriteLine(" Press [enter] to exit.");
+
+Console.ReadLine();
