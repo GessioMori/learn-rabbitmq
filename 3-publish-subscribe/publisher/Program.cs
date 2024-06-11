82b59ae77fdc74b3d4625e8e79e1b35f0287dcf0
Gessio Mori Neto <gessiomorin@gmail.com>
2024-06-11 20:33:02 -0300

    3-publish-subscribe

---
+++ 3-publish-subscribe/publisher/Program.cs
@@ -0,0 +1,51 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace publisher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new() { HostName = "localhost" };
+            using IConnection connection = factory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+
+            channel.ExchangeDeclare("logs", ExchangeType.Fanout);
+
+            Console.WriteLine("Sending logs. Press [ESC] to exit.");
+
+            int i = 0;
+
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    if (key.Key == ConsoleKey.Escape)
+                    {
+                        Console.WriteLine("Exiting");
+                        break;
+                    }
+                }
+
+                i++;
+
+                Thread.Sleep(1000);
+
+                string message = CreateLog();
+                byte[] body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish("logs", string.Empty, null, body);
+
+                Console.WriteLine($"{i} message(s) sent.");
+            }
+        }
+
+        static private string CreateLog()
+        {
+            Guid logId = Guid.NewGuid();
+            return $"Message: {logId}";
+        }
+    }
+}
---
+++ 3-publish-subscribe/subscriber/Program.cs
@@ -0,0 +1,50 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace subscriber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new() { HostName = "localhost" };
+            using IConnection connection = factory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+
+            channel.ExchangeDeclare("logs", ExchangeType.Fanout);
+
+            string queueName = channel.QueueDeclare().QueueName;
+
+            channel.QueueBind(queueName, "logs", string.Empty);
+
+            Console.WriteLine("Waiting for logs. Press [ESC] to exit.");
+
+            EventingBasicConsumer consumer = new(channel);
+            consumer.Received += (model, ea) =>
+            {
+                byte[] body = ea.Body.ToArray();
+                string message = Encoding.UTF8.GetString(body);
+                Console.WriteLine("Received message: " + message);
+            };
+
+            while (true)
+            {
+
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    if (key.Key == ConsoleKey.Escape)
+                    {
+                        Console.WriteLine("Exiting");
+                        break;
+                    }
+                }
+
+                channel.BasicConsume(queueName, true, consumer);
+
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
